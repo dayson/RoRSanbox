80048688b1a7f1139cfca475b01868fa8915a71c
Dan Ayson <danielayson@gmail.com>
2013-12-04 23:04:02 -0500

    End Chapter 2
    Change-Id: Id570eadd619a8f221a680e586a4fd58bdf5844b8

--- work/demo/app/controllers/say_controller.rb
+++ work/demo/app/controllers/say_controller.rb
@@ -1,7 +1,15 @@
 class SayController < ApplicationController
   def hello
+    @time = Time.now
+    @number = 2 + 3
   end
 
   def goodbye
+    @number = 5 + 5
+  end
+  
+  def filenames
+    @files = Dir.glob('*')
+    return @files
   end
 end
